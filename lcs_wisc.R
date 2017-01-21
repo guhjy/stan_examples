bd8ba145034bbe98ebfd7a6dc8f86b73bc346d85
Ross Jacobucci <rcjacobuc@gmail.com>
2017-01-21 11:58:14 -0800

    3 fac working

--- lcs_wisc.R
+++ lcs_wisc.R
@@ -12,9 +12,9 @@ X = as.matrix(X)
 N <- nrow(X)
 
 dat <- list(
-  "N" = N,
-  "X" = X,
-  "T" = 4)
+  N = N,
+  X = X,
+  t = 4)
 
 
 
@@ -25,51 +25,55 @@ dat <- list(
 
 stanmodel <- "
 data {
-int N; 
-int T; 
-matrix[N,T] X; 
+  int N; 
+  int t; 
+  vector[t] X[N]; 
 } 
 parameters {
-vector[2] beta;  
-real pi;
-real<lower=0> sigma; 
-cov_matrix[2] phi;
+  vector[2] beta;  
+  //real pi;
+  real<lower=0> sigma;
+  cov_matrix[2] phi;
 } 
-//transformed parameters {
-//cov_matrix[2] phi;
-//phi = diag_matrix(rep_vector(1.0,2)); 
-//} 
-
-model {
-matrix[N,2] s;
-matrix[N,T] y;
-matrix[N,T-1] d;
+transformed parameters {
+  vector[2] s[N];
+  vector[t] y[N];
+  vector[t-1] d[N];
 
 for (i in 1:N){
-s[i,1:2] ~ multi_normal(beta, phi);
-X[i,1]~ normal(y[i,1],pow(sigma, -0.5));
-y[i,1] = s[i,2];
+  y[i,1] = s[i,2];
 
-for (t in 2:T){
-X[i,t] ~ normal(y[i,t], pow(sigma, -0.5));
-d[i,t-1] = pi*y[i,t-1]+s[i,1];
-y[i,t] = d[i,t-1]+y[i,t-1];
+  for (tt in 2:t){
+    d[i,tt-1] = s[i,1];
+    y[i,tt] = d[i,tt-1]+y[i,tt-1];
+  }
 }
+  
+} 
+
+model {
+
+for (i in 1:N){
+  s[i,1:2] ~ multi_normal_prec(beta, phi);
+  X[i,1] ~ normal(y[i,1],pow(sigma, -0.5));
+  
+
+  for (tt in 2:t){
+    X[i,tt] ~ normal(y[i,tt], pow(sigma, -0.5));
+  }
 }
 
-sigma ~ gamma(1,10);
-pi~normal(0,10);
+sigma ~ gamma(.001,.001);
+//pi~normal(0,2);
 beta[1]~normal(-2,10);
-beta[2]~normal(20,10);
-phi~wishart(10,diag_matrix(rep_vector(1.0,2)));
+beta[2]~normal(20,.10);
+phi~wishart(2,diag_matrix(rep_vector(1.0,2)));
 }
 "
 
 
 library(rstan)
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
 
-fit <- stan(model_code = stanmodel, 
-        model_name = "LCS", 
-          data = dat,chains=1)
+fit <- stan(model_code = stanmodel, model_name = "LCS", 
+          data = dat,chains=1,
+        pars=c("beta","sigma"))
