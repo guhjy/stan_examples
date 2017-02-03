7e33e7d15106b99da970e0198abac46e2e8a693c
Ross Jacobucci <rcjacobuc@gmail.com>
2017-02-03 13:02:50 -0800

    mimic working

--- stan_mimic.R
+++ stan_mimic.R
@@ -4,8 +4,9 @@ HS <- HolzingerSwineford1939[complete.cases(HolzingerSwineford1939),]
 
 
 mod <- "
-f1 =~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
+f1 =~ NA*x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
 f1 ~ sex + grade + ageyr
+f1~~1*f1
 "
 out <- sem(mod, HS)
 summary(out)
@@ -27,16 +28,17 @@ dat <- list(
 mod.stan <-"
 data{
 int N; // sample size
-vector[9] X[N]; // data matrix of order [N,P]
-vector[3] cov[N]; // data matrix of order [N,P]
+matrix[N,9] X; // data matrix of order [N,P]
+matrix[N,3] cov; // data matrix of order [N,P]
 }
 
 parameters{
 vector[N] FS; // factor scores, matrix of order [N,D]
-real SD;
-vector[9] sigma;
+vector<lower=0>[9] sigma;
 vector[8] lam;
+vector[9] alpha;
 vector[3] beta;
+real<lower=0> psi;
 }
 
 transformed parameters{
@@ -46,31 +48,32 @@ vector[N] mu2;
 
 
 for(i in 1:N){
-mu[i,1] = 1*FS[i];
-mu[i,2] = lam[1]*FS[i];
-mu[i,3] = lam[2]*FS[i];
-mu[i,4] = lam[3]*FS[i];
-mu[i,5] = lam[4]*FS[i];
-mu[i,6] = lam[5]*FS[i];
-mu[i,7] = lam[6]*FS[i];
-mu[i,8] = lam[7]*FS[i];
-mu[i,9] = lam[8]*FS[i];
+mu[i,1] = alpha[1] + 1*FS[i];
+mu[i,2] = alpha[2] + lam[1]*FS[i];
+mu[i,3] = alpha[3] + lam[2]*FS[i];
+mu[i,4] = alpha[4] + lam[3]*FS[i];
+mu[i,5] = alpha[5] + lam[4]*FS[i];
+mu[i,6] = alpha[6] + lam[5]*FS[i];
+mu[i,7] = alpha[7] + lam[6]*FS[i];
+mu[i,8] = alpha[8] + lam[7]*FS[i];
+mu[i,9] = alpha[9] + lam[8]*FS[i];
 
-mu2[i] = beta[1]*cov[i,1] + beta[2]*cov[i,2] + beta[3]*cov[i,3];
+mu2[i] =  beta[1]*cov[i,1] + beta[2]*cov[i,2] + beta[3]*cov[i,3];
 
 }
 }
 
 model{
-SD ~ gamma(2,2);
 sigma ~ gamma(2,2);
-beta ~ normal(0,1);
+//beta ~ normal(0,1);
+alpha ~ normal(0,1);
+psi ~ gamma(2,2);
 
 for(i in 1:N){  
   for(j in 1:9){
-    X[i] ~ normal(mu[i],pow(sigma[j],0.5));
+    X[i,j] ~ normal(mu[i,j],pow(sigma[j],0.5));
   }
-FS[i] ~ normal(mu2[i],SD);
+FS[i] ~ normal(mu2[i],psi);
 }
 }
 "
@@ -78,4 +81,6 @@ FS[i] ~ normal(mu2[i],SD);
 
 fa.model=stan(model_code=mod.stan,
               data = dat,chains=1,
-              pars=c("sigma","lam","beta","SD"))
+              pars=c("sigma","lam","beta","psi","alpha"))
+
+print(fa.model)
