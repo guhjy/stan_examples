0a354133ddd72138d3d642139371a00f520164e5
Ross Jacobucci <rcjacobuc@gmail.com>
2017-06-02 14:53:37 -0700

    residuals3 working

--- stan_lgm_wisc_residuals3.R
+++ stan_lgm_wisc_residuals3.R
@@ -49,37 +49,50 @@ N <- nrow(X)
 dat2 <- list(
   N = N,
   X = X,
-  t = 4)
+  t = 4,
+  tt = rep(0,4))
 
 
 mod.stan <-"
 data{
 int N; // sample size
 int t; 
+vector[4] tt;
 vector[t] X[N]; // data matrix of order [N,P]
 }
 
 parameters{
 vector[2] FS[N]; // factor scores, matrix of order [N,D]
 cholesky_factor_cov[2] Rho; // correlation matrix between factors
+cholesky_factor_cov[4] Rho2; // correlation matrix between factors
 vector[2] M;
-vector[4] sig;
+//vector[4] sig;
 //real alpha;
-matrix[N,1] init_sigma;
-matrix[N,3] sigma; // residual score matrix
+
+
+//vector[4] lv_sigma[N]; 
 }
 
 transformed parameters{
+
 vector[t] mu[N];
 matrix[2,2] Sd_d;
+matrix[4,4] Sd_d2;
 Sd_d = multiply_lower_tri_self_transpose(Rho);
+Sd_d2 = multiply_lower_tri_self_transpose(Rho2);
+
 
 
 for(i in 1:N){
-mu[i,1] = FS[i,1] + 1*FS[i,2] + init_sigma[i,1];
-mu[i,2] = FS[i,1] + 2*FS[i,2] + sigma[i,1]; 
-mu[i,3] = FS[i,1] + 4*FS[i,2] + sigma[i,2];
-mu[i,4] = FS[i,1] + 6*FS[i,2] + sigma[i,3];
+mu[i,1] = FS[i,1] + 1*FS[i,2] + sigma[i,1];
+mu[i,2] = FS[i,1] + 2*FS[i,2] + sigma[i,2]; 
+mu[i,3] = FS[i,1] + 4*FS[i,2] + sigma[i,3];
+mu[i,4] = FS[i,1] + 6*FS[i,2] + sigma[i,4];
+
+//sigma[i,1] = lv_sigma[i,1];
+//sigma[i,2] = lv_sigma[i,2];
+//sigma[i,3] = lv_sigma[i,3];
+//sigma[i,4] = lv_sigma[i,4];
 
 //for(j in 1:4){
 //sigma[i,1] = alpha*init_sigma[i,1];
@@ -94,17 +107,16 @@ model{
 M[1] ~ normal(20,10);
 M[2] ~ normal(5,10);
 Rho ~ lkj_corr_cholesky(2.0);
+Rho2 ~ lkj_corr_cholesky(5.0);
 //alpha ~ normal(0,1);
-sig ~ normal(30,30);
+
 
 for(i in 1:N){    
 for(j in 1:4){
-X[i,j] ~ normal(mu[i,j],.0000001); // fix variance to .0000001
+X[i,j] ~ normal(mu[i,j],.001); // fix variance to .0000001
 }
-init_sigma[i,1] ~ normal(0,sig[1]);
-for(j in 1:3){
-sigma[i,j] ~ normal(0,sig[j+1]);
-}
+
+sigma[i] ~ multi_normal(tt,Sd_d2);
 
 FS[i] ~ multi_normal(M, Sd_d);
 }
@@ -112,7 +124,7 @@ FS[i] ~ multi_normal(M, Sd_d);
 "
 
 fa.model=stan(model_code=mod.stan,iter=10000,warmup=2000,
-              data = dat2,chains=1,thin=5,
-              pars=c("M","Sd_d","sig"))
+              data = dat2,chains=1,
+              pars=c("M","Sd_d","Sd_d2"))
 
 fa.model
